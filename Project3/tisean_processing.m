13f86a573610eb7cd073a5fc1d41fdf611f80e3e
jcwong2 <jcwong2@ualberta.ca>
2019-03-25 23:56:53 -0600

    Added delay embedding function to convert time series dataset to the form (delay vectors, next observations).

--- Project3/tisean_processing.m
+++ Project3/tisean_processing.m
@@ -44,13 +44,25 @@ if (f_fnn > FNN_THRESH)
     disp('Fraction of false nearest neighbours did not meet threshold (laser)\n');
 end
 
+% Get dimensionality of feature vectors for each dataset
+glass_m = glass_d * glass_tau;
+laser_m = laser_d * laser_tau;
+
+% Convert datasets into (delay vectors, next observation)
+[glass_inputs, glass_targets] = delay_embed(glass, glass_m, glass_tau);
+[laser_inputs, laser_targets] = delay_embed(laser, laser_m, laser_tau);
+
+% Write delay embedded datasets into .csv files
+csvwrite('glass_delay.csv', [glass_inputs glass_targets]);
+csvwrite('laser_delay.csv', [laser_inputs laser_targets]);
+
+%===FUNCTIONS===%
 % Determines optimal time delay based on finding the first local minimum of
 % the mutual information between a sample and its sample t steps after
 function tau = get_tau(mi_info)
 % Iterate through the mutual information results and find first local min
 min_val = inf;
 tau = 0;
-
 for i = 1:size(mi_info, 1)
     if (mi_info(i, 2) < min_val)
         min_val = mi_info(i, 2);
@@ -58,6 +70,29 @@ for i = 1:size(mi_info, 1)
     else
         break;  % Mutual information is starting to increase
     end
+end
+
+% Check that a local minimum was reached
+if i == size(mi_info, 1)
     disp('Local minimum never reached\n');
 end
+end
+
+% Obtain time-series dataset for neural network input by converting the
+% data into delay vectors and next observations
+function [inputs, targets] = delay_embed(time_series, m, tau)
+max_time = length(time_series);
+
+% Initialize input indices and target index, along with arrays to hold
+% inptus and targets
+input_ind = 1:tau:tau*m;
+target_ind = tau*(m+1);
+
+for i = 1:(max_time-target_ind+1)
+    inputs(i, :) = time_series(input_ind);
+    targets(i) = time_series(target_ind);
+    input_ind = input_ind + 1;
+    target_ind = target_ind + 1;
+end
+targets = targets';
 end
